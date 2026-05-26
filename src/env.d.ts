4df364c8eface72d4dfb6f7e81e53585796c607d
40gestao-cloud <4.0gestao@gmail.com>
2026-05-25 21:29:37 -0500

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,6 +9,8 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_KEY_2?: string;
   readonly VITE_SUPABASE_URL_3?: string;
   readonly VITE_SUPABASE_KEY_3?: string;
+  readonly VITE_SUPABASE_URL_4?: string;
+  readonly VITE_SUPABASE_KEY_4?: string;
 }
 
 interface ImportMeta {
